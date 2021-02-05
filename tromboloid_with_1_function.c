8407ff889282f3c4b03160d1de63d896f15dfb80
ENG19CS0031-ANANYA-BR <76058653+ENG19CS0031-ANANYA-BR@users.noreply.github.com>
2021-02-05 20:43:09 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -5,8 +5,9 @@ int main()
 float h,d,b,vol;
 printf("enter the height ,depth,breadth respectively");
 scanf("%f%f%f",&h,&d,&b);
-vol=(1/3)((h*b*d)+(d/b));
+vol=(0.333333)*((h*b*d)+(d/b));
 printf("volume of tromboloid of given %f , %f, %f is %f ",h,d,b,vol);
 return 0;
 }
+
 
