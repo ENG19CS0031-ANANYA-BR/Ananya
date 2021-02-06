db4f3bc04aecd1bcb4ad5ea70d3ff677a373709b
ENG19CS0031-ANANYA-BR <76058653+ENG19CS0031-ANANYA-BR@users.noreply.github.com>
2021-02-06 11:19:13 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -2,15 +2,16 @@
 #include<stdio.h>
 int main()
 {
-int n,count,num,sum=0;
-printf("enter the total number of numbers\n");
-scanf("%d",&n);
-printf("enter the numbers");
-for(count=1;count<=n;count++)
-{
-scanf("%d",&num);
-sum=sum+num;
-}
-printf("sum of %d numbers is %d",n,sum);
-return 0;
+   int n,count;
+   float num,sum=0;
+   printf("enter the total number of numbers\n");
+   scanf("%d",&n);
+   printf("enter the numbers");
+   for(count=1;count<=n;count++)
+      {
+         scanf("%f",&num);
+         sum=sum+num;
+      }
+   printf("sum of %d numbers is %f",n,sum);
+   return 0;
 }
