06a4aa393721d7f79f65c0d24657978bf6f0f519
ENG19CS0031-ANANYA-BR <76058653+ENG19CS0031-ANANYA-BR@users.noreply.github.com>
2021-02-07 12:14:20 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,10 +1,10 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-float three_d( float x1,float x2,float y1,float y2,float z1,float z2)
+float dist( float x1,float x2,float y1,float y2)
 {
     float d;
-    d= sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1)));
+    d= sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
     return d;
 }
 float input()
@@ -20,14 +20,12 @@ void output(float a)
 }
 int main()
 {
-    float a1,a2,b1,b2,c1,c2,d1,d2;
+    float a1,a2,b1,b2,d;
     a1=input();
     a2=input();
     b1=input();
     b2=input();
-    c1=input();
-    c2=input();
-    d=three_d(a1,a2,b1,b2,c1,c2);
+    d=dist(a1,a2,b1,b2);
     output(d);
     return 0;
 }
