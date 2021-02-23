8136bbc6a7ac852ff9c19346323e80ea2aa60e71
ENG19CS0031-ANANYA-BR <76058653+ENG19CS0031-ANANYA-BR@users.noreply.github.com>
2021-02-23 11:08:10 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,57 +2,57 @@
 #include<stdio.h>
 struct frac
 {
-int  numerator;
-int  denominator;
+    int  numerator;
+    int  denominator;
 };
 
 struct frac sum(struct frac a,struct frac b)
 {
-struct frac res;
-res.denominator=a.denominator*b.denominator;
-res.numerator=(a.numerator*b.denominator)+(b.numerator*a.denominator);
-int gcd=hcf(res.numerator,res.denominator);
-res.denominator=(res.denominator)/gcd;
-res.numerator=(res.numerator)/gcd;
-return res;
+    struct frac res;
+    res.denominator=a.denominator*b.denominator;
+    res.numerator=(a.numerator*b.denominator)+(b.numerator*a.denominator);
+    int gcd=hcf(res.numerator,res.denominator);
+    res.denominator=(res.denominator)/gcd;
+    res.numerator=(res.numerator)/gcd;
+    return res;
 }
 
 int hcf(int a,int b)
 {
-int gcd=1;
-int i=1;
+    int gcd=1;
+    int i=1;
 do
 {
-if(a%i==0&&b%i==0)
-gcd=i;
-i++;
+    if(a%i==0&&b%i==0)
+    gcd=i;
+    i++;
 } while (i<=a&&i<=b);
-return gcd;
+    return gcd;
 }
 
 struct frac input()
 {
-struct frac a;
-printf("Enter the numerator\n");
-scanf("%d",&a.numerator);
-printf("Enter the denominator\n");
-scanf("%d",&a.denominator);
-return a;
+    struct frac a;
+    printf("Enter the numerator\n");
+    scanf("%d",&a.numerator);
+    printf("Enter the denominator\n");
+    scanf("%d",&a.denominator);
+    return a;
 }
 
 
 void output(struct frac a,struct frac b,struct frac c)
 {
-printf("The sum of %d/%d and %d/%d is %d/%d",a.numerator,a.denominator,b.numerator,b.denominator,c.numerator,c.denominator);
+    printf("The sum of %d/%d and %d/%d is %d/%d",a.numerator,a.denominator,b.numerator,b.denominator,c.numerator,c.denominator);
 }
 
 
 int main()
 {
-struct frac a,b,c;
-a=input();
-b=input();
-c=sum(a,b);
-output(a,b,c);
-return 0;
+    struct frac a,b,c;
+    a=input();
+    b=input();
+    c=sum(a,b);
+    output(a,b,c);
+    return 0;
 }
